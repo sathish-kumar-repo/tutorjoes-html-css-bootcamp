0fe59bb8ece3114cec5dd6f804ae0cc79c5751c6
sathish-kumar-repo <sathishkumar.mailhub@gmail.com>
2025-07-01 16:27:15 +0530

    gd

--- 00 Temp/36_Responsive_Navbar_Design/js/script.js
+++
@@ -1,9 +0,0 @@
-window.onload = function(){
-    const menu_btn=document.querySelector('.right-bar');
-    const mobile_nav=document.querySelector('.mobile-nav');
-
-    menu_btn.addEventListener('click',function(){
-        menu_btn.classList.toggle('is-active');
-        mobile_nav.classList.toggle('is-active');
-    });
-}
